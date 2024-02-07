5a6aea845a491665fcdd3278772376739eb32707
Mar7155 <142651337+Mar7155@users.noreply.github.com>
2024-02-06 21:37:50 -0600

    Add files via upload
    o

---
+++ ButtonsFactoryWeb - copia (2)/javascript/scroll.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: "auto",
+	centeredSlides: true,
+	spaceBetween: 30,
+	grabCursor: true,
+	loop: true,
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+  });
